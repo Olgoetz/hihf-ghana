c80897e6203677d21384ce39c0b6269e40eaa525
Oliver Götz <olivergoetz@Olivers-MacBook-Air-2022.local>
2024-02-22 12:56:05 +0100

    border order adjusted

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -43,7 +43,7 @@ export async function getFiles() {
 
 const GET_BOARD_MEMBERS = gql`
   query BoardMembers {
-    boardMembers {
+    boardMembers(orderBy: order_ASC) {
       id
       image {
         url
