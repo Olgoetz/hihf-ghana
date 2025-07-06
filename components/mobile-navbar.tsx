aef8c43e550a882e0dee8943d62a5a4b88d93413
Oliver Götz <goetzoliver89@gmail.com>
2025-07-06 20:38:10 +0100

    translations

--- components/mobile-navbar.tsx
+++ components/mobile-navbar.tsx
@@ -16,20 +16,17 @@ import { navLinks } from "./nav-links";
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import LanguageSwitcher from "./language-switcher";
+
 import { useTranslations } from "next-intl";
 
 export function MobileNavBar() {
   const [isOpen, setIsOpen] = useState(false);
-  const router = useRouter();
-  const onClick = (href: string) => {
-    router.push(href);
-  };
+  const t = useTranslations("navlinks");
 
   return (
     <div className="md:hidden">
-      <Sheet open={isOpen}>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <div className="flex items-center w-full justify-between">
           <SheetTrigger asChild>
             <Button variant="ghost" onClick={() => setIsOpen(true)}>
@@ -47,26 +44,27 @@ export function MobileNavBar() {
         </div>
         <SheetContent side="left" className="w-full flex flex-col h-full p-0">
           <div className="flex-1 flex flex-col items-center justify-center">
-            <ul className="flex flex-col items-center text-lg gap-8 w-full">
+            <ul className="flex flex-col items-center text-lg gap-4 w-full">
               {navLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.path}
-                  onClick={() => setIsOpen(false)}
-                  className="w-full text-center text-hih-primary uppercase hover:bg-hih-primary/10 hover:rounded-lg p-4 "
-                >
-                  <li className="w-full">{link.title}</li>
-                </Link>
+                <li className="w-full" key={link.title}>
+                  <Link
+                    href={link.path}
+                    className="w-full block text-center text-hih-primary uppercase hover:bg-hih-primary/10 hover:rounded-lg p-4 "
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {t(link.title)}
+                  </Link>
+                </li>
               ))}
             </ul>
-            <div className="flex justify-center w-full mt-8">
+            <div className="flex justify-center w-full mt-4">
               <LanguageSwitcher />
             </div>
           </div>
-          <SheetClose className="absolute right-4 top-4">
-            <Button variant="ghost" onClick={() => setIsOpen(false)}>
+          <SheetClose asChild>
+            <button className="absolute right-4 top-4" aria-label="Close">
               <X className="h-4 w-4" />
-            </Button>
+            </button>
           </SheetClose>
         </SheetContent>
       </Sheet>
--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -1,7 +1,45 @@
+locals {
+  hihf_contributors = [
+    "phlipp1902",
+  ]
+}
+
 resource "github_repository" "this" {
   name        = "hihf-ghana"
   description = "Website for Help is Here for Ghana e.V."
+  visibility  = "private"
+}
 
-  visibility = "public"
 
+
+# Protect the main branch of the foo repository. Additionally, require that
+# the "ci/travis" context to be passing and only allow the engineers team merge
+# to the branch.
+
+resource "github_branch_protection" "main" {
+  repository_id = github_repository.this.node_id
+  # also accepts repository name
+  # repository_id  = github_repository.example.name
+  pattern = "main"
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+    dismiss_stale_reviews           = true
+  }
+
+
+}
+
+# Add a team to the organization
+resource "github_team" "contributors" {
+  name        = "hihf-contributors"
+  description = "Contributors to the Help is Here for Ghana project"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "hihf_contributors" {
+  for_each = toset(local.hihf_contributors)
+  team_id  = github_team.contributors.id
+  username = each.key
+  role     = "member"
 }
